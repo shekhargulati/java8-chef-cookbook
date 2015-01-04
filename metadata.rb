4a04cb8941ab2b294a08fc3644f260ab30795bc7
Shekhar Gulati <shekhargulati84@gmail.com>
2015-01-04 13:15:21 +0530

    Java 8 Chef Cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'java8'
+maintainer       'Shekhar Gulati'
+maintainer_email 'shekhargulati84@gmail.com'
+license          'all_rights'
+description      'Installs/Configures java8'
+long_description 'Installs/Configures java8'
+version          '0.1.0'
+
+depends 'java'
